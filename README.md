237ef85f22cb79ae50ccf1a8c1f7983a244c56b3
Lewis Nixon <lewcfn@gmail.com>
2016-12-08 18:30:49 +0000

    Adding ava test runner and some tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eslint-config-boxdeluxe
-> See [Shareable eslint configs](http://eslint.org/docs/developer-guide/shareable-configs) for more details
+> See [Shareable eslint configs](http://eslint.org/docs/developer-guide/shareable-configs) for 
+more details
+
+> Supports es2017 features
---
+++ test/test.js
@@ -0,0 +1,45 @@
+import test from 'ava';
+import isPlainObj from 'is-plain-obj';
+import eslint from 'eslint';
+import tempWrite from 'temp-write';
+
+const hasRule = (errors, ruleId) => errors.some(error => error.ruleId === ruleId);
+
+function runEslint(str, conf) {
+  const linter = new eslint.CLIEngine({
+    useEslintrc: false,
+    configFile: tempWrite.sync(JSON.stringify(conf))
+  });
+
+  return linter.executeOnText(str).results[0].messages;
+}
+
+test('main', t => {
+  const conf = require('../');
+
+  t.true(isPlainObj(conf));
+  t.true(isPlainObj(conf.rules));
+
+  const errors = runEslint('\'use strict\';\nconst foo = "bar"\n', conf);
+  t.true(hasRule(errors, 'quotes'));
+});
+
+test('esnext', t => {
+  const conf = require('../');
+
+  t.true(isPlainObj(conf));
+  t.true(isPlainObj(conf.rules));
+
+  const errors = runEslint('var foo = true;\n', conf);
+  t.true(hasRule(errors, 'no-var'));
+});
+
+test('esnext es2016', t => {
+  const conf = require('../');
+
+  t.true(isPlainObj(conf));
+  t.true(isPlainObj(conf.rules));
+
+  const errors = runEslint('let unused; const x = async () => {\n\tawait Promise.resolve({b: 1, ...x});\n};\n', conf);
+  t.true(hasRule(errors, 'no-unused-vars'));
+});
