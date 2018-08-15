e2476e65f08f7b558400ae9b6329a87f72a7cf0d
Lewis Nixon <lewcfn@gmail.com>
2018-08-15 14:50:07 +0200

    update parser options

--- index.js
+++ index.js
@@ -2,19 +2,13 @@
 
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 6,
     sourceType: 'module',
   },
   "env": {
     "browser": true,      // browser global variables.
     "node": true,         // Node.js global variables and Node.js-specific rules.
-    "amd": false,         // defines require() and define() as global variables as per the amd spec.
-    "mocha": false,       // adds all of the Mocha testing global variables.
-    "jasmine": true,      // adds all of the Jasmine testing global variables for version 1.3 and 2.0.
-    "phantomjs": true,    // phantomjs global variables.
-    "jquery": false,      // jquery global variables.
-    "prototypejs": false, // prototypejs global variables.
-    "shelljs": false,     // shelljs global variables.
+    "es6": true,          // ES6 Globals
   },
 
   "rules": {
